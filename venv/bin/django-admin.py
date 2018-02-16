aee6c1be9a77281ed8adf67717dab1c807aaa63b
lucia <lucia.iosa@outlook.com>
2018-02-16 10:49:42 +0100

    Cambie HTML para la pagina

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/opinno/proyectos/djangogirlsproyect/venv/bin/python3
+#!/home/opinno/proyectos/djangogirlsproject/venv/bin/python2
 from django.core import management
 
 if __name__ == "__main__":
